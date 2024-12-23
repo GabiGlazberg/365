c52382ac6dd86c6354e0bbec179f82367d57e485
gabiglazberg <gglazberg@gmail.com>
2024-12-23 17:25:57 +0200

    Create VPC and RT association

--- vpc.tf
+++ vpc.tf
@@ -49,3 +49,20 @@ resource "aws_route_table" "private_rt" {
     Name = "private_rt"
   }
 }
+
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "main_igw"
+  }
+}
+
+resource "aws_route_table_association" "public_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
